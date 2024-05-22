c94687ef71d5504fd3e4a0b7557f4f7f77ce209a
itsahadul99 <itsahadul999@gmail.com>
2024-05-22 23:23:45 +0600

    added delete api for user

--- index.js
+++ index.js
@@ -31,6 +31,13 @@ async function run() {
             const result = await userCollection.find().toArray();
             res.send(result)
         })
+        // delete a user
+        app.delete('/users/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await userCollection.deleteOne(query)
+            res.send(result)
+        })
         app.post('/users', async(req, res) => {
             const userData = req.body;
             const query = {email: userData.email}
